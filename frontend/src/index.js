3841bcdafbe6b0e1b715c23a913013eca923ff31
yousef <yousef.alzaatari@gmail.com>
2021-11-24 21:15:41 +0300

    Header and Footer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// a theme from https://bootswatch.com/
+import './bootstrap.min.css'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
