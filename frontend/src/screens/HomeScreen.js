4128d86c883b6b61de542c6b81c7b7e31bcec8ea
yousef <yousef.alzaatari@gmail.com>
2021-11-25 15:46:54 +0300

    HomeScreen Product Listing

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Product from '../components/Product'
+import products from "../products";
+
+const HomeScreen = () => {
+    return (
+        <>
+            <h1>Latest Products</h1>
+            <Row>
+                {products.map((product) => (
+                    <Product product={product}/>
+                ))}
+            </Row>
+        </>
+    );
+};
+
+export default HomeScreen;
