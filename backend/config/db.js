88c1d76af51e8897053b3eced7fc9a55302e1980
yousef <yousef.alzaatari@gmail.com>
2021-11-28 15:05:41 +0300

    Add Colors to Console

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,10 +9,10 @@ const connectDB = async () => {
         });
 
         console.log(
-            `MongoDB Connected: ${conn.connection.host}`
+            `MongoDB Connected: ${conn.connection.host}`.cyan.underline
         );
     } catch (error) {
-        console.error(`Error: ${error.message}`);
+        console.error(`Error: ${error.message}`.red.underline.bold);
         process.exit(1);
     }
 };
