ed549bfc35c1271388bcd2ec40599cf791b2ee37
yousef <yousef.alzaatari@gmail.com>
2021-12-08 20:00:49 +0300

    Adding PayPal Payments

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,14 @@ const app = express()
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.send("API is running...")
+  res.send("API is running...")
 })
 
 app.use("/api/products", productRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/orders", orderRoutes)
+
+app.get("/api/config/paypal", (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
 
 app.use(notFound)
 
@@ -30,9 +32,8 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 app.listen(
-    PORT,
-    console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-            .bold
-    )
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 )
