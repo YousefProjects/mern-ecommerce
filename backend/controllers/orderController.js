b69bb00a199b18abc03fd2ea2593a9a2832e7377
yousef <yousef.alzaatari@gmail.com>
2021-12-08 17:36:56 +0300

    Order Screen & Check for Order

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -39,11 +39,15 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id).populate('user', 'name email')
-  if(order){
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  )
+  if (order) {
     res.json(order)
   } else {
-    throw new Error('Order not found')
+    res.status(404)
+    throw new Error("Order not found")
   }
 })
 
