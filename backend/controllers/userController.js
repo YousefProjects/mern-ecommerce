2eaa805e50d2ded54f47dd7e9e39dc141bc0effc
yousef <yousef.alzaatari@gmail.com>
2021-12-10 20:07:14 +0300

    Admin User Delete

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -79,7 +79,7 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name
     user.email = req.body.email || user.email
-    if (req.body.password){
+    if (req.body.password) {
       user.password = req.body.password
     }
     const updatedUser = await user.save()
@@ -105,4 +105,25 @@ const getUsers = asyncHandler(async (req, res) => {
   res.json(users)
 })
 
-export { authUser, getUserProfile, registerUser, updateUserProfile, getUsers }
+// @desc    Delete user
+// @route   DELETE /api/users/:id
+// @access  Private/Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+  if (user) {
+    await user.remove()
+    res.json({ message: "User removed" })
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
+
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+}
