b21a40cfbf54cbf8bcbbcee08e9271a754666f03
yousef <yousef.alzaatari@gmail.com>
2021-12-15 15:47:08 +0300

    Admin Order List

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -63,7 +63,7 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
       id: req.body.id,
       status: req.body.status,
       update_time: req.body.update_time,
-      email_address: req.body.payer.email_address
+      email_address: req.body.payer.email_address,
     }
     const updatedOrder = await order.save()
     res.json(updatedOrder)
@@ -81,4 +81,18 @@ const getMyOrders = asyncHandler(async (req, res) => {
   res.json(orders)
 })
 
-export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders }
+// @desc    Get all orders
+// @route   GET /api/orders/
+// @access  Private/Admin
+const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name")
+  res.json(orders)
+})
+
+export {
+  addOrderItems,
+  getOrderById,
+  updateOrderToPaid,
+  getMyOrders,
+  getOrders,
+}
